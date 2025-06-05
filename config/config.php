080be1753cadb76e74481a91e22f690d91e56799
morph3us777 <morpheuscassian@gmail.com>
2025-06-05 11:43:34 -0500

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,4 +1,4 @@
 <?php
 //↓↓↓↓↓↓↓↓↓↓↓↓↓↓ AQUI SE CONFIGURA EL TOKEN DEL CANAL DE LA INFO ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓.
-define('DISCORD_WEBHOOK_URL', 'https://discordapp.com/api/webhooks/1379454315886018720/t4OZ6nHNMP8qBVI_nw7o54fluJgSn8JtMV4pmIsi0tg7uIaJdiqKTAWC_OJ0OX5mlnWp');
+define('DISCORD_WEBHOOK_URL', 'https://discord.com/api/webhooks/1380093594248941629/iQ7VBF8JIoqlxgapUVlrbk-3cST1JziLxtnzjcrbddDZRBWXy8CM5ZxX1Oa2g4mPD4kS');
 ?>
