f68a80240369d023c3819a3e4a7e65895b53df54
morph3us777 <morpheuscassian@gmail.com>
2025-06-07 08:53:40 -0500

    Delete config directory

--- config/config.php
+++
@@ -1,4 +0,0 @@
-<?php
-//↓↓↓↓↓↓↓↓↓↓↓↓↓↓ AQUI SE CONFIGURA EL TOKEN DEL CANAL DE LA INFO ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓.
-define('DISCORD_WEBHOOK_URL', 'https://discord.com/api/webhooks/TOKEN_DE_TU_CANAL_DE_INFO');
-?>
